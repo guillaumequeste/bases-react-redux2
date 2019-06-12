86da40622fb9e18e46606164ff73412cd0542863
Guillaume Queste <guillaume.queste@laposte.net>
2019-06-12 16:21:31 +0200

    ajout api 2

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import store from './store'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import API from './components/API'
+import APIdeux from './components/APIdeux'
 import './index.css'
 
 const app = (
     <Provider store={store}>
         <App />
+        <hr />
         <div className="api">
             <API />
+        </div>
+        <hr />
+        <div className="apideux">
+            <APIdeux />
         </div>
     </Provider>
 )
@@ -19,4 +25,7 @@ ReactDOM.render(app, document.getElementById('root'));
 
 /* yarn add react
     yarn add react-redux
-    yarn add redux-logger */
+    yarn add redux-logger
+    
+    yarn add axios --save
+    link bootstrap dans public/index.html */
